5dd3053033a72e270563111651bf390fc4194d00
Yang Wang <119640315+yangwang825@users.noreply.github.com>
2024-06-19 22:22:36 +0100

    Add files via upload

--- _scripts/main.js
+++ _scripts/main.js
@@ -73,5 +73,6 @@ $(function() {
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
+  sr.reveal('.publishment', { viewFactor: 0.05 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
